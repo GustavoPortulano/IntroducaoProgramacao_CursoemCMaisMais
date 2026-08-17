23515d78c7b7504d093ec92431aa853337a13076
GustavoPortulano <gfa.portulano@gmail.com>
2026-08-17 17:56:15 -0300

    Correção do exercício 4.3

--- Capitulo4_Exercicios/Exercicio3.cpp
+++ Capitulo4_Exercicios/Exercicio3.cpp
@@ -3,7 +3,7 @@
 // calcule f(x), sendo que:
 // f(x) = x + 2x² se g(x) > 10
 // f(x) = 10 se g(x) <= 10
-// g(x) 5 se h(x) <= 5
+// g(x) = 5 se h(x) <= 5
 // g(x) = h(x) se h(x) > 5
 // h(x) = x² + 3x - 20 
 
@@ -17,27 +17,20 @@ int main(){
 
     int x;
     int g = 0;
-    int h = 0;
 
     cout << "Digite um valor para g(x): " << endl;
     cin >> x;
+    
 
     if (x > 10){
         g = x + 2*x*x;
-        cout << g;
-    } else if (x <= 10){
-        x = 10;
-        cout << g;
+        cout << "Valor da função f(x): " << g;
+    } else if ((x <= 10) and (x > 5)){
+        g = 10;
+        cout << "Valor da função f(x): " << g;
     } else if (x <=5){
          g = 5;
-         cout << g;
-    } else if (x > 5){
-        h = x;
-        g = h;
-    } else if (x){
-        h = x*x +3*x -20;
-        cout << h;
+         cout << "Valor da função f(x): " << g;
     }
-
     return 0;
 }
